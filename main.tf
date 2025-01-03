dc854b529de02ae3c308f91100c8636f6e61096e
naveenpola09 <153384644+naveenpola09@users.noreply.github.com>
2025-01-03 11:26:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0ddc798b3f1a5117e"
+    image_id           = "ami-0ca9fb66e076a6e32"
     instance_type = "t2.micro"
-    key_name = "mustafakey-pair"
+    key_name = "bhagi"
     
 }
    
